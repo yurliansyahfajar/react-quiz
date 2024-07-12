48bce57cf74beed03ddadf4b57d44b047fca2fa6
yurliansyahf <yurliansyahf@gmail.com>
2024-07-13 01:34:23 +0700

    add next question function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Loader from "./components/Loader";
 import Error from "./components/Error";
 import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
+import NextButton from "./components/NextButton";
 
 const intialState = {
   questions: [],
@@ -13,6 +14,7 @@ const intialState = {
   status: "loading",
   index: 0,
   answer: null,
+  points: 0
 };
 
 function reducer(state, action) {
@@ -21,23 +23,35 @@ function reducer(state, action) {
       return {
         ...state,
         questions: action.payload,
-        status: "ready",
+        status: "ready"
       };
     case "dataFailed":
       return {
         ...state,
-        status: "error",
+        status: "error"
       };
     case "start":
       return {
         ...state,
         status: "active",
-        index: 0,
+        index: 0
       };
     case "newAnswer":
+      const question = state.questions.at(state.index);
+
       return {
         ...state,
         answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points
+      };
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null
       };
     default:
       throw new Error("Action is unknown");
@@ -56,11 +70,11 @@ export default function App() {
         const data = await res.json();
         dispatch({
           type: "dataReceived",
-          payload: data,
+          payload: data
         });
       } catch (error) {
         dispatch({
-          type: "dataFailed",
+          type: "dataFailed"
         });
       }
     }
@@ -77,11 +91,14 @@ export default function App() {
           <StartScreen numberQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
